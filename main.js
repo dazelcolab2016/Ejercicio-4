4b5945a7347a62e28ec7baf1fd5695939eff12a3
Zeldina Chalco <dazelcolab2016@gmail.com>
2016-12-20 10:45:58 -0500

    run ejercicio-4

---
+++ main.js
@@ -0,0 +1,11 @@
+var tiempo, costoMinuto, costoLlamada;
+tiempo = prompt("Por favor ingresa tiempo");
+costoMinuto = prompt("Por favor ingresa costoMinuto");
+costoLlamada = parseInt(tiempo) * parseInt(costoMinuto);
+alert (costoLlamada);
+
+
+
+
+
+
